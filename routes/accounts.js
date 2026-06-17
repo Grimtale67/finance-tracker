a3573c05e1750ac08c3cb20f4c730e567f4a33ad
sambu62 <s23107142@stu.edu.tw>
2026-06-17 20:53:16 +0800

    fix error

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const Record = require('../models/Record')
+const Record = require('../models/record')
 const authMiddleware = require('../middleware/authMiddleware')
 
 // Protect all routes below
